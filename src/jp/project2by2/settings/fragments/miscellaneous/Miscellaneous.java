c7c1a06e46a376cac1ccacfa9c98885e968ecbbe
lahaina <nattolecats@gmail.com>
2024-09-05 21:58:36 +0900

    2byTricks: Add feature of directly pif.json downloading * Also import some JP translates

--- src/jp/project2by2/settings/fragments/miscellaneous/Miscellaneous.java
+++ src/jp/project2by2/settings/fragments/miscellaneous/Miscellaneous.java
@@ -6,12 +6,16 @@
 package jp.project2by2.settings.fragments.miscellaneous;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.DownloadManager;
 import android.content.ContentResolver;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.Handler;
 import android.os.SystemProperties;
 import android.util.Log;
@@ -41,36 +45,81 @@ public class Miscellaneous extends SettingsPreferenceFragment implements
 
     private static final String TAG = "Miscellaneous";
 
-    private static final String KEY_PIF_JSON_FILE_PREFERENCE = "pif_json_file_preference";
+    private static final String KEY_PIF_JSON_MANAGE_PREFERENCE = "pif_json_manage_preference";
 
-    private Preference mPifJsonFilePreference;
+    private Preference mPifJsonManagePreference;
 
     private Handler mHandler;
+
+    private Context context;
+    private Resources resources;
+    private ContentResolver resolver;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.custom_settings_miscellaneous);
 
-        final Context context = getContext();
-        final ContentResolver resolver = context.getContentResolver();
+        context = getContext();
+        resources = context.getResources();
+        resolver = context.getContentResolver();
+
         final PreferenceScreen prefScreen = getPreferenceScreen();
-        final Resources resources = context.getResources();
 
         mHandler = new Handler();
 
-        mPifJsonFilePreference = findPreference(KEY_PIF_JSON_FILE_PREFERENCE);
+        mPifJsonManagePreference = findPreference(KEY_PIF_JSON_MANAGE_PREFERENCE);
     }
 
     @Override
     public boolean onPreferenceTreeClick(Preference preference) {
-        if (preference == mPifJsonFilePreference) {
-            Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-            intent.setType("application/json");
-            startActivityForResult(intent, 10001);
+        if (preference == mPifJsonManagePreference) {
+            onPifManageScreen(context);
             return true;
         }
         return super.onPreferenceTreeClick(preference);
+    }
+
+    private void onPifManageScreen(Context context) {
+        final String[] items = {
+            resources.getString(R.string.spoofing_pif_json_download_title),
+            resources.getString(R.string.spoofing_pif_json_select_title),
+        };
+        new AlertDialog.Builder(context)
+            .setTitle(resources.getString(R.string.spoofing_pif_manage_title))
+            .setItems(items, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    if (which == 0) {
+                        downloadPifJson(context);
+                    } else if (which == 1) {
+                        selectPifJson(context);
+                    }
+                }
+            })
+            .show();
+    }
+
+    private void downloadPifJson(Context context) {
+        DownloadManager downloadManager = (DownloadManager) context.getSystemService(Context.DOWNLOAD_SERVICE);
+        Uri uri = Uri.parse(resources.getString(R.string.spoofing_pif_json_download_url));
+        String mes = resources.getString(R.string.spoofing_pif_json_download_message);
+
+        DownloadManager.Request request = new DownloadManager.Request(uri)
+            .setTitle("pif.json")
+            .setDescription(mes)
+            .setMimeType("application/json")
+            .setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
+            .setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, "pif.json");
+        downloadManager.enqueue(request);
+
+        Toast.makeText(context, resources.getString(R.string.spoofing_pif_json_download_message), Toast.LENGTH_LONG).show();
+    }
+
+    private void selectPifJson(Context context) {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("application/json");
+        startActivityForResult(intent, 10001);
     }
 
     @Override
@@ -104,7 +153,7 @@ public class Miscellaneous extends SettingsPreferenceFragment implements
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
-        final Context context = getContext();
+        context = getContext();
         final ContentResolver resolver = context.getContentResolver();
         return false;
     }
